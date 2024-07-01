856dc1d4281e7883f201c2d56e020aae236379cf
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-01 15:45:15 -0600

    Update placeHolders.js

--- scripts/placeHolders.js
+++ scripts/placeHolders.js
@@ -1,40 +1,44 @@
 // placeHolders.js
 // load components that will be on all pages
 
-// navbar.js
-
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarPlaceholder = document.getElementById('navbar-placeholder');
-
-    // Create a new XMLHttpRequest object
-    const xhr = new XMLHttpRequest();
-
-    // Configure it to fetch the navbar.html file
-    xhr.open('GET', 'components/navbar.html', true);
-
-    // Setup onload callback
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Insert the received HTML into the placeholder
-            navbarPlaceholder.innerHTML = xhr.responseText;
-        } else {
-            console.error('Failed to load navbar.html');
-        }
-    };
-
-    // Send the request
-    xhr.send();
-});
-
-
-// footer.js
-
-function includeFooter() {
-    const footerPlaceholder = document.getElementById('footer-placeholder');
-    if (footerPlaceholder) {
-        footerPlaceholder.innerHTML = `<object type="text/html" data="components/footer.html"></object>`;
-    }
+const addComponent = ({placeholderID,htmlURL}) =>{
+    document.addEventListener("DOMContentLoaded", function() {
+        const componentPlaceholder = document.getElementById({placeholderID});
+    
+        // Create a new XMLHttpRequest object
+        const xhr = new XMLHttpRequest();
+    
+        // Configure it to fetch the navbar.html file
+        xhr.open('GET', {`components/${htmlURL}`}, true);
+    
+        // Setup onload callback
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                // Insert the received HTML into the placeholder
+                navbarPlaceholder.innerHTML = xhr.responseText;
+            } else {
+                console.error(`Failed to load ${htmlURL}`);
+            }
+        };
+    
+        // Send the request
+        xhr.send();
+    });
 }
 
-// Call the function to include footer when the page loads
-window.addEventListener('DOMContentLoaded', includeFooter);
+const components = [
+    { 
+        id:"navbar-placeholder", // navbar.js
+        url:"navbar.html"
+    },
+    {
+        id:"footer-placeholder", // footer.js
+        url:"footer.html"
+    }
+]
+
+// call it
+components.forEach(({id,url})=>addComponent(id,url))
+
+
+
