515fdd21c974f45b174ff45e08e3d6168c6a514f
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-02 16:04:32 -0600

    Update loadProjectDetails.js

--- scripts/loadProjectDetails.js
+++ scripts/loadProjectDetails.js
@@ -19,13 +19,13 @@ document.addEventListener('DOMContentLoaded', function() {
         qrapp: {
             title: 'QRapp',
             description: 'QRapp is designed to be a fun, engaging and social experience for users. You choose the way you play: You can design to be a casual collector of some of the highest, lowest or coolest codes or compete against friends or other QR Hunters for top positions one the many leaderboards. As you play, you might become aware of te fact that codes are everywhere! ',
-            images: ['qrapp1.jpg', 'qrapp2.jpg'],
+            images: ['qrapp1.jpg', 'qrapp2.jpg','qrapp3.jpg', 'qrapp4.jpg','qrapp5.jpg', 'qrapp6.jpg',],
             videos: ['QRAPP.webm'],
             technologies: [
-                'Android Studio: Frontend, business logic and conencting to Backend',
+                'Android Studio: IDE, Frontend, business logic and connection to Backend',
+                'Figma: Wireframing and Storyboarding',
                 'Firebase/Firestore: Data Storage and Management',
                 'Java: Formal programming language',
-                'Figma: Wireframing and Storyboarding',
                 'GitHub: Version Control, Documentation and Task management.'
             ],
             contributions: [
@@ -43,13 +43,34 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         aranimal: {
             title: 'AR Animal Identification',
-            description: 'Detailed description of Project 2.',
-            images: [],
+            description: "The AR Animal Identification app is part of Aquatic Biosphere many projects designed to spread awareness and raise funds to support, uphold and maintain the Albertan Biosphere. Aquatic Biosphere is a non-profit with a singluar mission: 'To educate, inspire and empower people to play an active role in the conservation of our aquatic ecosystems.' We designed this app exactly for this reason. It is designed to accompany a trailer with a vinyl sticker illustrate the various organisms of the ALbertan Bioshpere. Users will be able to identify these organisms and learn about them from interactive games and activities. The app is also designed to allow for effortless customiztion for admin roles to add, remove and modify information presented to users. Create a profile and experience the wonders of the Albertan Biospere. ",
+            images: ['aranimal1.jpg','aranimal2.jpg','aranimal3.jpg','aranimal4.jpg','aranimal5.jpg',],
             videos: ['ARANIMAL.webm'],
-            technologies: ['Flutter', 'Poetry'],
-            contributions: 'Full-stack development with React and Node.js.',
+            technologies: [
+                'Android Studio/Visual Studio: IDE',
+                'Figma: Wireframing and Storyboarding',
+                'Flutter: Frontend, Business Logic, Connection to Backend',
+                'FastAPI: Backend, Creating API endpoints to and from Database and for Image Recognition',
+                'PostgreSQL: Data Storage and Management',
+                'Dart, Python, SQL: Formal programming/query languages',
+                'OpenCV: Computer Vision Library used for Image Processing and Image Identification',
+                'Poetry: Package Management',
+                'Cybera: Cloud Infrastructure and services',
+                'Uvicorn: Serve/deploy internal image recoginition and User APIs',
+                'GitHub: Version Control, Documentation and Task management.',
+                'VuePress.js: Creation of a simple website for Full Documentation and Project Planning'
+            ],
+            contributions: [
+                "Ensuring all our work is properly documented on github + VuePress.js and github wiki pages",
+                "Keeping meeting minutes for all meetings and documenting them",
+                "Storyboarding all use cases and user experiences",
+                "Image Recognition process and methods using OpenCV and integration of system into app",
+                "Optimisation of Image Processing",
+                "Games and Activities Design and Architecture",
+                "Admin page setup",
+            ],
             bestFeatures: 'Scalability and real-time updates.',
-            rating: 4.8
+            rating: 4.4
         },
         // project3: {
         //     title: 'Project 3 Title',
@@ -57,7 +78,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //     images: ['project3-img1.jpg', 'project3-img2.jpg'],
         //     videos: [],
         //     technologies: ['Angular', 'Spring Boot'],
-        //     contributions: 'Frontend using Angular, backend with Spring Boot.',
+        //     contributions: 'Frontend using Angular, backend with Spring Boot.','Full-stack development with React and Node.js.',Scalability and real-time updates'.,
         //     bestFeatures: 'Security features and robust architecture.',
         //     rating: 4.3
         // }
