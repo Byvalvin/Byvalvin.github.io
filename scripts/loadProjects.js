7cf6575b12789175a8b414f6e622119a48ea8815
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-02 21:20:55 -0600

    Update loadProjects.js

--- scripts/loadProjects.js
+++ scripts/loadProjects.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
             id: 'qrapp',
             title: 'QRapp',
             description: 'A QR Code Hunting App.',
-            images: ['qrapp4.jpg'],
+            images: ['qrapp6.jpg'],
             detailsPage: 'project-details.html?id=qrapp'
         },
         {
