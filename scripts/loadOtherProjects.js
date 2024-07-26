0302e3fe63ec43c924e2f238c462a4e29d9277c5
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-26 17:45:57 -0600

    Update loadOtherProjects.js

--- scripts/loadOtherProjects.js
+++ scripts/loadOtherProjects.js
@@ -1,7 +1,8 @@
 // loadOtherProjects.js
 
 document.addEventListener('DOMContentLoaded', function() {
-    const projectsList = document.getElementById('other-project-list');
+    const projectList = document.getElementById('other-project-list');
+    const descriptionLength = 150; // Adjust as needed
 
     // Fetch projects data from JSON file
     fetch('projects/other/otherprojects.json')
@@ -9,19 +10,23 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             // Iterate over projects and create list items
             data.projects.forEach(project => {
+                const truncatedDescription = project.description.length > descriptionLength 
+                    ? project.description.substring(0, descriptionLength) + '...' 
+                    : project.description;
+
                 const div = document.createElement('div');
                 div.classList.add('project-card');
                 div.innerHTML = `
                     <div class="project-card-header">
                         <h2>${project.name}</h2>
-                        <p>${project.description}</p>
+                        <p>${truncatedDescription}</p>
                     </div>
                 `;
                 // Add a click event listener to navigate to project details page
                 div.addEventListener('click', () => {
                     window.location.href = project.otherDetailsPage; // Navigate to project details page
                 });
-                projectsList.appendChild(div);
+                projectList.appendChild(div);
             });
         })
         .catch(error => console.error('Error loading projects:', error));
