c6bcb479bd5b66d7cacd162e4c00a4bbbc61343b
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-30 23:28:09 -0600

    Update feelingLucky.js

--- scripts/feelingLucky.js
+++ scripts/feelingLucky.js
@@ -5,7 +5,11 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const feelingLuckyButton = document.querySelector('.logo-img'); // Use the logo as the Feeling Lucky button
-    if(feelingLuckyButton)console.log("button herere");
+    if(feelingLuckyButton){
+        console.log("button herere");
+    }else{
+        console.log("button NOT herere");
+    }
     const body = document.body;
     const fonts = ['Poppins:wght@300;400;600', 'Roboto:wght@400;700', 'Arial', 'Verdana'];
     const themes = ['default', '1', '2', '3'];
