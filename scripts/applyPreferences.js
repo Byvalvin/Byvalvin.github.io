6eb50ffafb2d9316ce89609ccd8ab544ca80d99d
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-07 17:00:58 -0600

    Update applyPreferences.js

--- scripts/applyPreferences.js
+++ scripts/applyPreferences.js
@@ -14,6 +14,7 @@ const applyStoredPreferences = () => {
     } else {
         console.log('No theme preference found.');
     }
+    
 
     // Apply stored font if available
     if (storedFont) {
@@ -23,7 +24,14 @@ const applyStoredPreferences = () => {
     } else {
         console.log('No font preference found.');
     }
-};
+
+    // Change logo image based on theme
+    const feelingLuckyButton = document.querySelector(.logo-img);
+    if (storedTheme === '21') {
+        feelingLuckyButton.src = 'images/omnitrix1.jpg'; // Set to Omnitrix image
+    } else {
+        feelingLuckyButton.src = 'images/Byvalvin.jpeg'; // Set to default logo
+    }};
 
 // Apply preferences when the DOM content is fully loaded(will be loaded by now)
 applyStoredPreferences();
