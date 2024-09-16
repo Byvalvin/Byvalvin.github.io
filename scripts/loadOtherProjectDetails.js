773cdd7fa8ed020c2ff361cc9b3a8a42fbced5f9
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-09-16 13:29:28 -0600

    Update loadOtherProjectDetails.js

--- scripts/loadOtherProjectDetails.js
+++ scripts/loadOtherProjectDetails.js
@@ -44,12 +44,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 const githubLinkHTML = project.githubLink ? 
                     `<a class="code-link" href="${project.githubLink}" target="_blank" rel="noopener noreferrer">
                         <i class="fab fa-github"></i>
+                        <span class="btn-text">View on Github</span>
+                        
                     </a>` : '';
 
                 // Render the "Try it out" link if tryItOutLink exists
                 const tryItOutLinkHTML = project.tryItOutLink ? 
                     `<a class="try-it-out-link" href="${project.tryItOutLink}" target="_blank" rel="noopener noreferrer">
                         <i class="fa-solid fa-play"></i>
+                        <span class="btn-text">Try It Out</span>
                     </a>` : '';
                 
                 return `
