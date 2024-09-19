095810756703e24362e55480c57c8014811230ac
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-09-19 15:05:59 -0600

    Update applyPreferences.js

--- scripts/applyPreferences.js
+++ scripts/applyPreferences.js
@@ -7,18 +7,28 @@ const applyStoredPreferences = () => {
     const storedTheme = localStorage.getItem('selectedTheme');
     const storedFont = localStorage.getItem('selectedFont');
 
-    // Apply stored theme if available
-    if (storedTheme) {
+    // Check current time
+    const currentHour = new Date().getHours();
+    const darkTime = 18;
+    const darkThemes = ['4', '5', '8', '12', '16', '20', '22'];
+
+    // Apply dark theme if it's 6 PM or later
+    if (currentHour >= darkTime && !storedTheme) {
+        // Set to a default dark theme (e.g., '8')
+        const randDarkThemeIndex = Math.floor(Math.random()*darkThemes.length);
+        body.setAttribute('data-theme', darkThemes[randDarkThemeIndex]); // Choose one of your darker themes
+        localStorage.setItem('selectedTheme', 'darkThemes[randDarkThemeIndex]); // Save this preference
+        console.log('Dark theme applied by default: ', darkThemes[randDarkThemeIndex]);
+    } else if (storedTheme) {
+        // Apply stored theme if available
         body.setAttribute('data-theme', storedTheme);
         console.log(`Theme applied: ${storedTheme}`);
     } else {
         console.log('No theme preference found.');
     }
-    
 
     // Apply stored font if available
     if (storedFont) {
-        // Ensure the font format is correct for applying directly to the style
         body.style.fontFamily = storedFont;
         console.log(`Font applied: ${storedFont}`);
     } else {
@@ -31,8 +41,8 @@ const applyStoredPreferences = () => {
         feelingLuckyButton.src = 'images/omnitrix1.jpg'; // Set to Omnitrix image
     } else {
         feelingLuckyButton.src = 'images/Byvalvin.jpeg'; // Set to default logo
-    }};
+    }
+};
 
-// Apply preferences when the DOM content is fully loaded(will be loaded by now)
+// Apply preferences when the DOM content is fully loaded
 applyStoredPreferences();
-
