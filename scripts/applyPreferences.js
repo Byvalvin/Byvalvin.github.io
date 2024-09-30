0e3b0d742bbc3951828dd3a2d8b344d8d952b314
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-09-30 16:40:05 -0600

    Update applyPreferences.js

--- scripts/applyPreferences.js
+++ scripts/applyPreferences.js
@@ -9,8 +9,8 @@ const applyStoredPreferences = () => {
 
     // Check current time
     const currentHour = new Date().getHours();
-    const lightTime;
-    const lightThemes = ['0','1','2','3','7', '9', '10', '11', '13', '14', '15', '17', '18', '19','21','22', '23', '24', '26']
+    const lightTime = 12;
+    const lightThemes = ['0','1','2','3','7', '9', '10', '11', '13', '14', '15', '17', '18', '19','21','22', '23', '24', '26'];
     const darkTime = 18;
     const darkThemes = ['4', '5', '6', '8', '12', '16', '20', '22', '27'];
 
