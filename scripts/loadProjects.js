2c4965461b7ec177c205209bab1c26f837cb82f8
Daniel Asimiakwini <asimiakw@ualberta.ca>
2024-10-02 21:51:25 -0600

    9+--

--- scripts/loadProjects.js
+++ scripts/loadProjects.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             projectSection.addEventListener('click', (event) => {
                 if (event.target.tagName !== 'A') { // If not clicking on the link
                     showSection(event, 'projectDetails'); // Show project details section
-                    window.history.pushState({ sectionId: 'projectDetails', projectId: project.id }, '', `#project-details?id=${project.id}`);
+                    //window.history.pushState({ sectionId: 'projectDetails', projectId: project.id }, '', `#project-details?id=${project.id}`);
                     //loadProjectDetails(project.id); // Load the project details
                 }
             });
