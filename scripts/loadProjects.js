c6c49a4f9fe4187520056ca325739857f96479ac
Daniel Asimiakwini <asimiakw@ualberta.ca>
2024-10-02 21:13:45 -0600

    8++++

--- scripts/loadProjects.js
+++ scripts/loadProjects.js
@@ -49,9 +49,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     <h2>${project.title}</h2>
                     <p>${project.description}</p>
                     <div class="project-images">${projectImages}</div>
-                    <a href="#project-details?id=${project.id}" class="btn btn-primary" onclick="showSection(event, 'projectDetails'); loadProjectDetails('${project.id}');">View Details</a>
+                    <a href="#project-details?id=${project.id}" class="btn btn-primary" onclick="showSection(event, 'projectDetails');">View Details</a>
                 </div>
             `;
+        
         
 
             // Add a click event listener to navigate to project details page
